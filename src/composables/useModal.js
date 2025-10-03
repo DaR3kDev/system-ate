fd046f1de7510e10d38561cda43ad6945389b2b4
DaR3kDev <villegasdeveloper@outlook.es>
2025-10-03 15:07:08 -0500

    chore: remove deprecated vue.js files and clean up project structure
    This commit removes all deprecated .vue.js files and unused utility files that were part of the previous Vue.js implementation. The cleanup includes:
    - Removing all component files
    - Deleting unused stores and composables
    - Cleaning up router and main application files
    - Removing TypeScript build info file

--- src/composables/useModal.js
+++
@@ -1,6 +0,0 @@
-export function useModal(modalRef) {
-    const open = () => modalRef.value?.showModal();
-    const close = () => modalRef.value?.close();
-    const toggle = () => (modalRef.value?.open ? modalRef.value.close() : modalRef.value?.showModal());
-    return { open, close, toggle };
-}
